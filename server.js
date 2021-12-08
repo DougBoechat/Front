3e93a51bc5418b8670f053ae9bd540099af7e52a
Douglas Boechat <douglas.souza@parceiros.odontoprev.com.br>
2021-12-08 19:49:30 -0300

    preparando para o heroku

--- server.js
+++ server.js
@@ -8,3 +8,5 @@ app.use(express.static(__dirname + "/dist/front"));
 app.get("/*", function(req, res) {
     res.sendFile(path.join(__dirname + "/dist/front/index.html"));
 });
+
+app.listen(process.env.PORT || 8080);
