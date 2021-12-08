7e67ce826cef34528399c305752ef6488f20c5a8
Douglas Boechat <douglas.souza@parceiros.odontoprev.com.br>
2021-12-08 19:45:30 -0300

    preparando para o heroku

--- server.js
+++ server.js
@@ -8,6 +8,3 @@ app.use(express.static(__dirname + "/dist/front"));
 app.get("/*", function(req, res) {
     res.sendFile(path.join(__dirname + "/dist/front/index.html"));
 });
-
-// Inicia a aplicação pela porta configurada
-app.listen(process.env.PORT || 8080);
